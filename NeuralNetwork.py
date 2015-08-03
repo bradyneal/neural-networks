e09fc6766bd22e73fba92b0a0530f869582a67dd
Brady Neal <bradyneal11@gmail.com>
2015-08-03 11:10:19 -0400

    fleshed out constructor

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,7 +1,13 @@
 __author__ = 'brady'
 
+import numpy as np
+
 
 class NeuralNetwork:
 
-    def __init__(self):
-        pass
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(size, 1) for size in sizes[1:]]
+        self.weights = [np.random.randn(size, previous_size) for
+                        size, previous_size in zip(size[1:], size[:-1])]

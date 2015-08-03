4ae90c14e8ca8a50b67f46b2c4648912bae2ad6a
Brady Neal <bradyneal11@gmail.com>
2015-08-03 18:20:08 -0400

    outlined mini-batch gradient descent

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,6 +1,7 @@
 __author__ = 'brady'
 
 import numpy as np
+import random
 
 
 class NeuralNetwork:
@@ -13,16 +14,30 @@ class NeuralNetwork:
                         size, previous_size in zip(sizes[1:], sizes[:-1])]
 
     def forward_propagate(self, input_layer):
-        a = to_column_vector(input_layer)
+        a = column_vector(input_layer)
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(w.dot(a) + b)
         return a
+
+    def mini_batch_gradient_descent(self, training_data, mini_batch_size,
+                                    learning_rate, num_iterations):
+        for iteration in range(num_iterations):
+            mini_batches = randomly_partition(training_data, mini_batch_size)
+            for mini_batch in mini_batches:
+                self.mini_batch_update(mini_batch, learning_rate)
+
+    def mini_batch_update(self, mini_batch, learning_rate):
+        pass
 
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
-def to_column_vector(l):
-    l = np.array(l)
-    l.shape = (len(l), 1)
-    return l
+def column_vector(L):
+    L = np.array(L)
+    L.shape = (len(L), 1)
+    return L
+
+def randomly_partition(L, partition_size):
+    random.shuffle(L)
+    return (L[i:i + partition_size] for i in range(0, len(L), partition_size))

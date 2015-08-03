d75c33997918e52634fdb74dd1ec831812af6a55
Brady Neal <bradyneal11@gmail.com>
2015-08-03 14:52:55 -0400

    fleshed out forward propogation

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,15 +5,24 @@ import numpy as np
 
 class NeuralNetwork:
 
-    def __init__(self, sizes, input=None):
-        assert input is None and sizes[0] == len(input), \
-            "Input size not compatible with sizes list"
+    def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.sizes = sizes
-        self.input = input
         self.biases = [np.random.randn(size, 1) for size in sizes[1:]]
         self.weights = [np.random.randn(size, previous_size) for
-                        size, previous_size in zip(size[1:], size[:-1])]
+                        size, previous_size in zip(sizes[1:], sizes[:-1])]
 
-    def forward_propogate(self, input=None):
-        
+    def forward_propogate(self, input):
+        a = to_column_vector(input)
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(w.dot(a) + b)
+        return a
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+def to_column_vector(l):
+    l = np.array(l)
+    l.shape = (len(l), 1)
+    return l

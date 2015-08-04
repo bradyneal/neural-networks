3bfb9cfabb5c251548c8534970a74c127055006d
Brady Neal <bradyneal11@gmail.com>
2015-08-04 10:53:56 -0400

    fleshed out mini-batch gradient descent

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -27,6 +27,22 @@ class NeuralNetwork:
                 self.mini_batch_update(mini_batch, learning_rate)
 
     def mini_batch_update(self, mini_batch, learning_rate):
+        total_bias_gradient = [np.zeros(b.shape) for b in self.biases]
+        total_weight_gradient = [np.zeros(w.shape) for w in self.weights]
+        for features, y in mini_batch:
+            bias_gradient, weight_gradient = self.back_propagate(features, y)
+            total_bias_gradient = add_lists(total_bias_gradient, bias_gradient)
+            total_weight_gradient = add_lists(total_weight_gradient,
+                                              weight_gradient)
+        mini_batch_size = len(mini_batch)
+        self.biases = gradient_descent_update(self.biases,
+                                              total_bias_gradient,
+                                              learning_rate, mini_batch_size)
+        self.weights = gradient_descent_update(self.weights,
+                                               total_weight_gradient,
+                                               learning_rate, mini_batch_size)
+
+    def back_propagate(self, features, y):
         pass
 
 
@@ -41,3 +57,10 @@ def column_vector(L):
 def randomly_partition(L, partition_size):
     random.shuffle(L)
     return (L[i:i + partition_size] for i in range(0, len(L), partition_size))
+
+def add_lists(L1, L2):
+    return [x1 + x2 for x1, x2 in zip(L1, L2)]
+
+def gradient_descent_update(initial, gradient, learning_rate, num_examples):
+    return [initial_x - (learning_rate / num_examples * derivative)
+            for initial_x, derivative in zip(initial, gradient)]
